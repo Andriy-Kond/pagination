9b4f04671817954a16d998ad3e8923aa27615b72
Andrii <andrii.kondratovich@gmail.com>
2024-05-30 08:31:48 +0300

    add styles

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // News API
 console.log("news API :>> ");
+const NEWS_API_KEY = "c439d9bd8a0f4e879b73f0b05ea17406";
+// const END_POINT = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=c439d9bd8a0f4e879b73f0b05ea17406";
+const END_POINT = "https://newsapi.org/v2/everything?";
+const options = {
+  headers: {
+    Authorization: NEWS_API_KEY,
+  },
+};
+
+const request = "cat";
+
+fetch(`${END_POINT}q=${request}&language=en&pageSize=10`, options)
+  .then(r => r.json())
+  .then(console.log);
